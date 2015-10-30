facc69bbcc7a4bf7eff43482dc061c49c99e4dbd
Boris Scheiman <bscheiman@gmail.com>
2015-10-30 11:44:39 -0600

    Extensions

---
+++ Conekta.Xamarin.PCL/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Conekta.Xamarin.PCL.Extensions {
+	public static class StringExtensions {
+		public static T FromJson<T>(this string str) {
+			return JsonConvert.DeserializeObject<T>(str);
+		}
+	}
+}
+
